9b5326306a176168b7752f17716a34373135d694
Shoibo <srabosti.k@gmail.com>
2024-09-27 12:41:31 +0600

    added loading part

--- client/src/api/queries/useProduct.jsx
+++ client/src/api/queries/useProduct.jsx
@@ -1,6 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { ProductService } from "../service";
 
+
 export const useProduct=()=> {
-    const products=ProductService.getProduct();
-    return {products};
+    const [products,setProduct]=useState([]);
+    const [isLoading,setisLoading]=useState(false);
+    const loadProducts= async()=> {
+        try {
+            setisLoading(true);
+            const products = await ProductService.getProduct();
+             setProduct(products);
+        }
+        catch(error) {
+            alert('Failed to load products!');
+            console.log(error);
+        }
+        finally {
+            setisLoading(false);
+        }
+        
+    }  
+   
+   
+    useEffect( ()=> {
+        loadProducts();
+    },[]);
+    return {products,isLoading};
 }
--- client/src/components/leftPanel/product.jsx
+++ client/src/components/leftPanel/product.jsx
@@ -1,34 +1,26 @@
-import axios from 'axios';
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Product_card } from "./product_card";
 import './product.css';
-//import { useProduct } from "../../api/queries/useProduct";
+
+import { useProduct } from "../../api/queries/useProduct";
 
 export function Product() {
-    const [products,setProduct]=useState([]);
-    useEffect(()=> {
-        axios.get('http://localhost:8000/api/products').then((res)=> {
-          setProduct(res.data);
-        }).catch((error)=> {
-           alert("Failed to Load Products!!");
-           console.log(error);
-        })
-       
-    },[]);
-   // 
+   
+   
 
 
-   //const {products}=useProduct();
+   const {products,isLoading}=useProduct();
       
-      // Example usage
-  // Outputs: External SSD 1TB
+   
       
    
     return (
         <div class="product-container">
-            {products.map((p) => (
-                <Product_card a={p}   />
-            ))}
+            {/* ///conditional rendering */}
+            {isLoading ? <h1>Loading Products.........</h1> :products.map((p) => (
+                <Product_card a={p}  />
+            )) }
+            
         </div>
 
     );
